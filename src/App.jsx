8e51b868906b2c64dbf3218a0c752918aced187d
feranmi Isacc <feranmiisaac622@gmail.com>
2025-11-29 11:30:26 +0100

    Creating the contact form

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Testimonuals from "./components/Testimonuals";
+import Contact from "./components/Contact";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <About />
       <Projects />
       <Testimonuals />
+      <Contact />
     </div>
   );
 };
